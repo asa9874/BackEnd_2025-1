c2d5d2cdb219167dba22a8e9497cae703267839f
asa9874 <pjkpjk1129@gmail.com>
2025-05-03 16:46:47 +0900

    feat:  introduce.html 파일 및 과제 엔드포인트추가

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -2,6 +2,7 @@ package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,4 +18,41 @@ public class HelloController {
     public String hello2() {
         return "hello";
     }
+
+    @GetMapping("/introduce")
+    public String introduce() {
+        return "introduce";
+    }
+
+    @GetMapping("/introduce2")
+    @ResponseBody
+    public String introduce2(@RequestParam("name") String name) {
+        return "hello " + name + "!";
+    }
+
+    @GetMapping("/json")
+    @ResponseBody
+    public Person json() {
+        Person person = new Person("허준기", 25);
+        return person;
+    }
+
+    public class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+
 }
