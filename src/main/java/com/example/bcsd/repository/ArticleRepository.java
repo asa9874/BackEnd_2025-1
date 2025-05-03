c22e6f6f64233c4e12c64e8c215c88a9affdb436
asa9874 <pjkpjk1129@gmail.com>
2025-05-03 16:23:15 +0900

    feat: ArticleRepository 및 articles 클래스 구현

--- src/main/java/com/example/bcsd/repository/ArticleRepository.java
+++ src/main/java/com/example/bcsd/repository/ArticleRepository.java
@@ -1,5 +1,7 @@
 package com.example.bcsd.repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Component;
@@ -8,15 +10,25 @@ import com.example.bcsd.model.Article;
 
 @Component
 public class ArticleRepository {
+    private List<Article> articles = new ArrayList<>(); // 데이터베이스 대용
+
+    public ArticleRepository() { // 초기 데이터
+        articles.add(new Article("제목1", "내용1"));
+    }
+
     public Optional<Article> findById(Long id) {
-        return Optional.empty();
+        return articles.stream()
+                       .filter(article -> article.getId().equals(id))
+                       .findFirst();
     }
 
     public void deleteById(Long id) {
-
+        articles.removeIf(article -> article.getId().equals(id));
     }
 
     public void save(Article article) {
-        
+        // 기존에 동일 id의 Article이 있다면 삭제후 추가(업데이트)
+        deleteById(article.getId());
+        articles.add(article);
     }
 }
