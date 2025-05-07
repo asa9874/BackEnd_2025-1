13b4d880470a879e52c7bfbc79e482eacd1fcce2
asa9874 <pjkpjk1129@gmail.com>
2025-05-07 12:10:17 +0900

    refact: ArticleResponseDto에서 setter 메서드 제거

--- src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
+++ src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
@@ -17,24 +17,12 @@ public class ArticleResponseDto {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContent() {
         return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 
     public static ArticleResponseDto from(Article article) {
