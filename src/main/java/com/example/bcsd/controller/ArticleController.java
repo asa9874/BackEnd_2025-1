9597c2df27daf464a1198d659e9c89b63e633fb3
asa9874 <pjkpjk1129@gmail.com>
2025-05-07 11:55:28 +0900

    feat: posts, articles 컨트롤러 추가

--- src/main/java/com/example/bcsd/controller/ArticleController.java
+++ src/main/java/com/example/bcsd/controller/ArticleController.java
@@ -1,22 +1,23 @@
 package com.example.bcsd.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bcsd.dto.RequestDto.ArticleCreateRequestDto;
 import com.example.bcsd.dto.RequestDto.ArticleUpdateRequestDto;
 import com.example.bcsd.dto.ResponseDto.ArticleResponseDto;
 import com.example.bcsd.service.ArticleService;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -28,13 +29,19 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
+    @GetMapping
+    public ResponseEntity<List<ArticleResponseDto>> getArticles(){
+        List<ArticleResponseDto> articles = articleService.getArticles();
+        return ResponseEntity.ok().body(articles);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<ArticleResponseDto> getArticle(@PathVariable Long id){
         ArticleResponseDto articleDto = articleService.getArticle(id);
         return ResponseEntity.ok().body(articleDto);
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<ArticleResponseDto> createArticle(@RequestBody ArticleCreateRequestDto requestDto){
         ArticleResponseDto articleDto = articleService.createArticle(requestDto.getTitle(), requestDto.getContent());
         return ResponseEntity.status(HttpStatus.CREATED).body(articleDto);
