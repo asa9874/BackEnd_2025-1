bd2b0a68cb9deafc192f068d80b6f731661c4d38
asa9874 <pjkpjk1129@gmail.com>
2025-05-09 21:41:10 +0900

    Fix: Dto 요구사항에 맞게 변경

--- src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
+++ src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
@@ -3,67 +3,36 @@ package com.example.bcsd.dto.ResponseDto;
 import com.example.bcsd.model.Article;
 
 public class ArticleResponseDto {
-    private Long id;
     private String title;
+    private String author;
+    private String date;
     private String content;
-    private Long boardId;
-    private Long memberId;
-    private String createdAt;
-    private String updatedAt;
 
-    public ArticleResponseDto(Long id, Long boardId, Long memberId, String title, String content,
-            String createdAt, String updatedAt) {
-        this.id = id;
-        this.boardId = boardId;
-        this.memberId = memberId;
+    public ArticleResponseDto(String title, String author, String date, String content) {
         this.title = title;
-        this.content = content;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public ArticleResponseDto(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
+        this.author = author;
+        this.date = date;
         this.content = content;
     }
-
-    public Long getId() {
-        return id;
-    }
-
     public String getTitle() {
         return title;
     }
-
+    public String getAuthor() {
+        return author;
+    }
+    public String getDate() {
+        return date;
+    }
     public String getContent() {
         return content;
     }
 
-    public Long getBoardId() {
-        return boardId;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public String getUpdatedAt() {
-        return updatedAt;
-    }
-
     public static ArticleResponseDto from(Article article) {
         return new ArticleResponseDto(
-                article.getId(),
-                article.getBoard().getId(),
-                article.getMember().getId(),
                 article.getTitle(),
-                article.getContent(),
+                article.getMember().getName(),
                 article.getCreatedAt().toString(),
-                article.getUpdatedAt().toString());
+                article.getContent()
+        );
     }
 }
