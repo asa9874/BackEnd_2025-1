15bc0334c9cf4bfd24c666fc544c498d0ef9505c
asa9874 <pjkpjk1129@gmail.com>
2025-05-09 21:22:30 +0900

    feat: Article 클래스 속성추가

--- src/main/java/com/example/bcsd/model/Article.java
+++ src/main/java/com/example/bcsd/model/Article.java
@@ -2,11 +2,25 @@ package com.example.bcsd.model;
 
 public class Article {
     private Long id;
+    private Long boardId;
+    private Long memberId;
     private String title;
     private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+    public Article() {
+    }
 
-    public Article(Long id,String title, String content) {
+    public Article(Long id, Long boardId, Long memberId, String title, String content) {
+        this.id = id;
+        this.boardId = boardId;
+        this.memberId = memberId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article(Long id, String title, String content) {
         this.id = id;
         this.title = title;
         this.content = content;
@@ -39,5 +53,37 @@ public class Article {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Long getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(Long boardId) {
+        this.boardId = boardId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
