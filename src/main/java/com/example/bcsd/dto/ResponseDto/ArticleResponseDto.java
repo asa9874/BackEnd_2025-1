138fd7e17c32cd1390f3f4e8eff05b5ffab81d79
asa9874 <pjkpjk1129@gmail.com>
2025-05-09 21:23:53 +0900

    feat: ArticleResponseDto 속성추가

--- src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
+++ src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
@@ -6,6 +6,21 @@ public class ArticleResponseDto {
     private Long id;
     private String title;
     private String content;
+    private Long boardId;
+    private Long memberId;
+    private String createdAt;
+    private String updatedAt;
+
+    public ArticleResponseDto(Long id, Long boardId, Long memberId, String title, String content,
+            String createdAt, String updatedAt) {
+        this.id = id;
+        this.boardId = boardId;
+        this.memberId = memberId;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 
     public ArticleResponseDto(Long id, String title, String content) {
         this.id = id;
@@ -25,7 +40,25 @@ public class ArticleResponseDto {
         return content;
     }
 
+    public Long getBoardId() {
+        return boardId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
     public static ArticleResponseDto from(Article article) {
-        return new ArticleResponseDto(article.getId(), article.getTitle(), article.getContent());
+        return new ArticleResponseDto(article.getId(), article.getTitle(), article.getContent(),
+                article.getBoardId(), article.getMemberId(), article.getCreatedAt().toString(),
+                article.getUpdatedAt().toString());
     }
 }
