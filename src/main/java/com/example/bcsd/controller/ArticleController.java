bf657186e78bc029ddb07ce6b7e8c192e92c8ffd
asa9874 <pjkpjk1129@gmail.com>
2025-05-13 21:19:38 +0900

    Fix: PathVar -> RequestParams 요구사항에 맞게 수정

--- src/main/java/com/example/bcsd/controller/ArticleController.java
+++ src/main/java/com/example/bcsd/controller/ArticleController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bcsd.dto.RequestDto.ArticleCreateRequestDto;
@@ -29,6 +30,12 @@ public class ArticleController {
     @GetMapping
     public ResponseEntity<List<ArticleResponseDto>> getArticles() {
         List<ArticleResponseDto> responseDtos = articleService.getArticles();
+        return ResponseEntity.ok().body(responseDtos);
+    }
+
+    @GetMapping(params = "boardId")
+    public ResponseEntity<List<ArticleResponseDto>> getArticlesByBoardId(@RequestParam Long boardId) {
+        List<ArticleResponseDto> responseDtos = articleService.getArticlesByBoardId(boardId);
         return ResponseEntity.ok().body(responseDtos);
     }
 
--- src/main/java/com/example/bcsd/controller/PostController.java
+++ src/main/java/com/example/bcsd/controller/PostController.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.bcsd.service.ArticleService;
 import com.example.bcsd.service.BoardService;
@@ -19,8 +19,8 @@ public class PostController {
     @Autowired
     private BoardService boardService;
 
-    @GetMapping("/{boardId}")
-    public String getPosts(@PathVariable(name = "boardId") Long boardId, Model model) {
+    @GetMapping
+    public String getPosts(@RequestParam Long boardId, Model model) {
         model.addAttribute("board", boardService.getBoard(boardId));
         model.addAttribute("articles", articleService.getArticlesByBoardId(boardId));
         return "posts";
