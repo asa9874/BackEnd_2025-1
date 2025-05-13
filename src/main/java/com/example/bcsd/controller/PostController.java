49da715edb23f481ecf43fd011b4be2eca51be52
asa9874 <pjkpjk1129@gmail.com>
2025-05-13 21:07:28 +0900

    Feat: posts 요구사항에 맞게 변경, DTO 변경

--- src/main/java/com/example/bcsd/controller/PostController.java
+++ src/main/java/com/example/bcsd/controller/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.bcsd.service.ArticleService;
@@ -18,9 +19,8 @@ public class PostController {
     @Autowired
     private BoardService boardService;
 
-    @GetMapping
-    public String getPosts(Model model) {
-        Long boardId = 1L; // 일단 이걸로 기준으로 board 가져온다고 생각함(과제 조건에 따라 Path로 안함)
+    @GetMapping("/{boardId}")
+    public String getPosts(@PathVariable(name = "boardId") Long boardId, Model model) {
         model.addAttribute("board", boardService.getBoard(boardId));
         model.addAttribute("articles", articleService.getArticlesByBoardId(boardId));
         return "posts";
--- src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
+++ src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
@@ -3,12 +3,19 @@ package com.example.bcsd.dto.ResponseDto;
 import com.example.bcsd.model.Article;
 
 public class ArticleResponseDto {
+    private Long id;
+    private Long board_id;
+    private Long author_id;
     private String title;
     private String author;
     private String date;
     private String content;
 
-    public ArticleResponseDto(String title, String author, String date, String content) {
+    public ArticleResponseDto(Long id, Long board_id, Long author_id, String title, String author, String date,
+            String content) {
+        this.id = id;
+        this.board_id = board_id;
+        this.author_id = author_id;
         this.title = title;
         this.author = author;
         this.date = date;
@@ -31,8 +38,23 @@ public class ArticleResponseDto {
         return content;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public Long getBoardId() {
+        return board_id;
+    }
+
+    public Long getAuthorId() {
+        return author_id;
+    }
+
     public static ArticleResponseDto from(Article article) {
         return new ArticleResponseDto(
+                article.getId(),
+                article.getBoard().getId(),
+                article.getMember().getId(),
                 article.getTitle(),
                 article.getMember().getName(),
                 article.getCreatedAt().toString(),
