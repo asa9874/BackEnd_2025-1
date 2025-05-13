c9ef7494dcfac09be6b41f9538b131c8f0b424c9
asa9874 <pjkpjk1129@gmail.com>
2025-05-13 21:12:34 +0900

    Feat: ArticleResponseDTO 요구사항에 맞게 변경

--- src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
+++ src/main/java/com/example/bcsd/dto/ResponseDto/ArticleResponseDto.java
@@ -4,12 +4,12 @@ import com.example.bcsd.model.Article;
 
 public class ArticleResponseDto {
     private Long id;
-    private Long board_id;
     private Long author_id;
+    private Long board_id;
     private String title;
-    private String author;
-    private String date;
     private String content;
+    private String created_date;
+    private String modified_date;
 
     public ArticleResponseDto(Long id, Long board_id, Long author_id, String title, String author, String date,
             String content) {
@@ -17,21 +17,13 @@ public class ArticleResponseDto {
         this.board_id = board_id;
         this.author_id = author_id;
         this.title = title;
-        this.author = author;
-        this.date = date;
         this.content = content;
+        this.created_date = date;
+        this.modified_date = date;
     }
 
     public String getTitle() {
         return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getDate() {
-        return date;
     }
 
     public String getContent() {
@@ -50,14 +42,23 @@ public class ArticleResponseDto {
         return author_id;
     }
 
+    public String getCreatedDate() {
+        return created_date;
+    }
+
+    public String getModifiedDate() {
+        return modified_date;
+    }
+
     public static ArticleResponseDto from(Article article) {
+        String updatedAt = article.getUpdatedAt() != null ? article.getUpdatedAt().toString() : article.getCreatedAt().toString();
         return new ArticleResponseDto(
                 article.getId(),
                 article.getBoard().getId(),
                 article.getMember().getId(),
                 article.getTitle(),
-                article.getMember().getName(),
+                article.getContent(),
                 article.getCreatedAt().toString(),
-                article.getContent());
+                updatedAt);
     }
 }
